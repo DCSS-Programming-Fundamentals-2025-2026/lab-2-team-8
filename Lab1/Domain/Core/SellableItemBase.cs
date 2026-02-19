4e7889d0477e5e7ea3d7352cd8ec3ce157e432cb
Marina <marinakravcenko833@gmil.com>
2026-02-19 15:17:30 +0200

    Tests2.

--- Lab1/Domain/Core/SellableItemBase.cs
+++ Lab1/Domain/Core/SellableItemBase.cs
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-﻿namespace Lab1.Domain.Core
-=======
 ﻿namespace Lab1.Domain.Core;
 
 public abstract class SellableItemBase
->>>>>>> d61e295 (Tests.)
 {
     public string Name { get; }
     public decimal BasePrice { get; set; }
     public SellableItemBase(string name, decimal basePrice)
     {
-<<<<<<< HEAD
-        public string Name { get; }
-        public decimal BasePrice { get; set; }
-        public SellableItemBase (string name, decimal basePrice)
-        {
-            Name = name;
-            BasePrice = basePrice;
-        }
-=======
         Name = name;
         BasePrice = basePrice;
->>>>>>> d61e295 (Tests.)
     }
     public SellableItemBase(string name)
     {
